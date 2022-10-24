36a809dc3afb07b081f2dd15c29438052ded8ece
yiru00 <eva890706@gmail.com>
2022-10-24 22:34:49 +0800

    作業

---
+++ Exec_RemoveCollectionItems/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //在集合中移除多個項目
+        List<product> products = new List<product>();
+        products.Add(new product() { productName = "apple" });
+        products.Add(new product() { productName = "banana" });
+        products.Add(new product() { productName = "orange" });
+
+        products.Remove(new product() { productName="orange"});
+        foreach (var item in products)
+        {
+            Console.WriteLine(item.productName);
+        }
+
+    }
+}
+class product
+{
+    public string? productName { get; set; }
+}
+
